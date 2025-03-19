337c5df04e0ec2e765a63da93f84e6d7ed2f9ed4
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-03-18 22:05:30 -0500

    Added PDF auto-fill

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import user_list, user_create, user_update, user_delete, upload_signature
 from . import views
+from .views import generate_filled_pdf 
 
 urlpatterns = [
     path('', user_list, name="user_list"),  # List all users
@@ -11,5 +12,9 @@ urlpatterns = [
     path('reactivate/<int:user_id>/', views.reactivate_user, name='reactivate_user'),
     
     path('upload-signature/', upload_signature, name="upload_signature"), #signature route
-    path('forms/', views.form_selection, name='form_selection')
+    path('forms/', views.form_selection, name='form_selection'),
+]
+
+urlpatterns += [
+    path('generate-form/<str:form_type>/', generate_filled_pdf, name='generate_filled_pdf'),
 ]
