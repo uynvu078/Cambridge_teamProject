aa17342c435e77dd44e741fa1e3630c42d3df193
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-04-02 16:47:57 -0500

    Updating Styles

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for user_management project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
@@ -28,7 +11,7 @@ from django.conf.urls.static import static
 def dashboard_view(request):
     return render(request, 'dashboard.html')
 def home_view(request):
-    return HttpResponse("<h1>Welcome to User Management</h1><p><a href='/dashboard/'>Go to Dashboard</a></p>")
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
